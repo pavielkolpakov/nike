b169f1f9ab0f67e26e83239433f8455c63eadd95
Paviel <paviel@saveatrain.com>
2023-11-19 00:15:34 +0200

    updating hero section

--- src/app/content/hero/hero.component.ts
+++ src/app/content/hero/hero.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./hero.component.css']
 })
 export class HeroComponent {
+
+  public bigShoeImage: string = '../../../assets/images/big-shoe1.png';
   public statistics = [
     { value: '1k+', label: 'Brands' },
     { value: '500+', label: 'Shops' },
@@ -15,15 +17,25 @@ export class HeroComponent {
   public shoes = [
     {
         thumbnail: '../../../assets/images/thumbnail-shoe1.svg',
-        bigShoe: '../../../assets/images/big-shoe1.svg',
+        bigShoe: '../../../assets/images/big-shoe1.png',
     },
     {
-        thumbnail: '../../../assets/images/thumbnail-shoe1.svg',
-        bigShoe: '../../../assets/images/big-shoe1.svg',
+        thumbnail: '../../../assets/images/thumbnail-shoe2.svg',
+        bigShoe: '../../../assets/images/big-shoe2.png',
     },
     {
-        thumbnail: '../../../assets/images/thumbnail-shoe1.svg',
-        bigShoe: '../../../assets/images/big-shoe1.svg',
+        thumbnail: '../../../assets/images/thumbnail-shoe3.svg',
+        bigShoe: '../../../assets/images/big-shoe3.png',
     },
   ];
+
+  handleClick(bigShoeImg: string) {
+    if (this.bigShoeImage !== bigShoeImg) {
+      this.changeBigShoeImange(bigShoeImg);
+    }
+  }
+
+  changeBigShoeImange(bigShoeImg: string) {
+    this.bigShoeImage = bigShoeImg;
+  }
 }
